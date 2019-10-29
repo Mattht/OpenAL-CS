3c8e950f269952bfffa084c6f7e9c2c4b0cc5fe0
Mattht <msellers@outlook.com>
2019-10-29 12:39:52 +0000

    Add HRTF

--- src/ALEXT.cs
+++ src/ALEXT.cs
@@ -60,6 +60,20 @@ namespace OpenAL
 
 		public const int AL_GAIN_LIMIT_SOFT =			0x200E;
 
+		public const int ALC_SOFT_HRTF = 			0x0001;
+		public const int ALC_HRTF_SOFT =			0x1992;
+		public const int ALC_DONT_CARE_SOFT =			0x0002;
+		public const int ALC_HRTF_STATUS_SOFT =			0x1993;
+		public const int ALC_HRTF_DISABLED_SOFT =		0x0000;
+		public const int ALC_HRTF_ENABLED_SOFT =		0x0001;
+		public const int ALC_HRTF_DENIED_SOFT =			0x0002;
+		public const int ALC_HRTF_REQUIRED_SOFT =		0x0003;
+		public const int ALC_HRTF_HEADPHONES_DETECTED_SOFT =	0x0004;
+		public const int ALC_HRTF_UNSUPPORTED_FORMAT_SOFT =	0x0005;
+		public const int ALC_NUM_HRTF_SPECIFIERS_SOFT =		0x1994;
+		public const int ALC_HRTF_SPECIFIER_SOFT =		0x1995;
+		public const int ALC_HRTF_ID_SOFT =			0x1996;
+
 		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern void alGetBufferSamplesSOFT(
 			uint buffer,
@@ -68,6 +82,17 @@ namespace OpenAL
 			int channels,
 			int type,
 			IntPtr data
+		);
+
+		/* device refers to an ALCdevice* */
+		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern IntPtr alcGetStringiSOFT(IntPtr device, int param, int index);
+
+		/* device refers to an ALCdevice* */
+		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern bool alcResetDeviceSOFT(
+			IntPtr device,
+			int[] attrList
 		);
 	}
 }
